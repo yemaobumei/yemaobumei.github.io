6438c1addcb17894f02c96cc4811416f3e1c4b63
yemaobumei <385131462@qq.com>
2016-09-17 17:21:57 +0800

    ceshi

--- src/components/Main.js
+++ src/components/Main.js
@@ -73,12 +73,18 @@ class ImgFigure extends React.Component {
 		}
 
 		return (
-			<figure className={className}  style={styleObj} onClick={this.handleClick}><img src={this.props.data.imageURL} alt={this.props.data.title}/>
+			<figure className={className}  style={styleObj} onClick={this.handleClick}><img className="img" src={this.props.data.imageURL} alt={this.props.data.title}/>
 			<figcaption>
 			<h2 className="img-title">{this.props.data.title}</h2>
 			          <div className="img-back" onClick={this.handleClick}>
 				<p>
-				{this.props.data.title}
+				{this.props.data.instagram}
+				</p>
+				<p>
+				{this.props.data.douban}
+				</p>
+				<p>
+				{this.props.data.desc}
 				</p>
 				</div>
 			</figcaption>
