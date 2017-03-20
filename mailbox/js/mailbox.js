3e33a9b6bc08617b21b8e3423ec7461aea60bf52
YeFan <yefan@YeFandeMacBook-Pro.local>
2017-03-20 17:10:01 +0800

    修复对象里闭包变量公用问题

--- mailbox/js/mailbox.js
+++ mailbox/js/mailbox.js
@@ -23,37 +23,43 @@ var Mailbox=
 
 	};
 
-	var flag=false;//标志是否可以删除tip
-	var opts = {};
+
 	var Mailbox = function(options){
-		opts = $.extend({}, defaults , options||{});
-		opts.style=$.extend({},defaults.style,options.style||{});
-		$container=$(opts.container);		
-		initPagination();
-		inputChange();
-		keyDown();
+		this.flag=false;//标志是否可以删除tip
+		this.opts = $.extend({}, defaults , options||{});
+		this.opts.style=$.extend({},defaults.style,options.style||{});
+		this.$container=$(this.opts.container);
+			this.initPagination();
+			this.inputChange();
+			this.keyDown();				
+
 	}
 	Mailbox.prototype.getValue=function(){
 		var value="";
-		$container.find('.tip .message').each(function(index,ele){
+		this.$container.find('.tip .message').each(function(index,ele){
 			value=value+$(ele).html()+';';
 		})
 		return value;
 	}
+	Mailbox.prototype.initPagination=initPagination;
+	Mailbox.prototype.inputChange=inputChange;
+	Mailbox.prototype.keyDown=keyDown;
 	return Mailbox;
 //-----------------------初始化分页等参数------------------------------------------------------------
 	function initPagination(){
-
+		var this_=this;//绑定原型对象this
 		//设置容器，输入框的宽度，高度保持一致。
-		$container.css({'width':opts.width,'height':opts.height,'background':opts.background,'line-height':opts['line-height']});
-		var $box=$('<div class=" added"></div><div class="inputbox"><input type="text" style="border:none;padding:0 5px;background:transparent" placeholder='+opts.info+'></div>');
-		$box.appendTo($container);
-		$box.css({'height':opts.style.height,'border':'none','display':'inline','line-height':opts.style.height});
-		$box.find('input').css({height:opts.height});
+		this_.$container.css({'width':this_.opts.width,'height':this_.opts.height,'background':this_.opts.background,'line-height':this_.opts['line-height']});
+		var $box=$('<div class=" added"></div><div class="inputbox"><input type="text" style="border:none;padding:0 5px;background:transparent" placeholder='+this_.opts.info+'></div>');
+		$box.appendTo(this_.$container);
+		$box.css({'height':this_.opts.style.height,'border':'none','display':'inline','line-height':this_.opts.style.height});
+		$box.find('input').css({height:this_.opts.height});
+
 		//容器clik事件，激活input输入框，去掉tip_click样式
-		$container.click(function(event){
+		this_.$container.click(function(event){
+
 			$(this).find('input').focus();
-			$container.find('.tip').removeClass('tip_click');
+			$(this).find('.tip').removeClass('tip_click');
 			event.stopPropagation();
 		})
 	}
@@ -61,14 +67,77 @@ var Mailbox=
 
 //--------------------输入框内容改变事件--------------------------------
 	function inputChange(){
-		$container.find('input').change(input)
+		var this_=this;//绑定原型对象this
+		this_.$container.find('input').change(input);
+	//--------------内容改变具体的响应函数----------------------------------
+		function input(e){
+				var $this_=$(this);
+				var text=$this_.val();		
+				var tips="";
+				if(text.length>0){
+					if(/;/.test(text)){
+						var res=text.split(';');//分号;分组	
+					}else{
+						var res=[text];
+					}
+					$this_.val('')//清空
+					for(var i=0;i<res.length;i++){
+						if(res[i]!=""){
+							if(this_.opts.validation(res[i])){
+								tips+='<div class="tip"><span class="message">'+res[i]+'</span><a class="del" href="javascript:void(0)" >x</a></div>'	
+							}else{
+								tips+='<div class="tip error"><span class="message">'+res[i]+'</span><a class="del error" href="javascript:void(0)" >x</a></div>'	
+						}
+							}
+					}
+					var $tips=$(tips);
+					$tips.appendTo(this_.$container.find('.added'));
+					// console.log(this.$container.find('.added'))
+					$tips.each(function(index,ele){//$tips==>[div.tip,div.tip,…………]jquery数组
+
+						//div.tip小框样式设置	
+						$(ele).css(this_.opts.style)
+
+						//tips的hover事件，边框颜色变化
+						$(ele).hover(function(){
+
+							$(ele).addClass('tip_hover');
+							$(ele).find('a').addClass('tip_hover');
+						},function(){
+							$(ele).removeClass('tip_hover');
+							$(ele).find('a').removeClass('tip_hover');
+						})
+						//tip的click事件
+						$(ele).click(function(event){
+							event.stopPropagation();//阻止冒泡
+							this_.$container.find('input').focus()
+							if($(this).hasClass('tip_click')){
+								$(this).removeClass('tip_click');
+							}else{
+								$(this).addClass('tip_click');
+							}					
+						})					
+					})
+					//删除每个小tip框事件
+					$tips.find('.del').each(function(index,ele){
+						$(ele).click(function(event){
+							$(this).closest('div.tip').remove();
+						})
+					})
+
+					//恢复输入框默认宽度
+					$this_.css({'width':this_.opts.style['input_width']});
+					this_.flag=true;//此时输入框内容为空,this.flag置为true；
+				}
+		}
+
 	}
 //--------------------键盘输入事件-------------------------------------
 	//绑定键盘事件
 	function keyDown(){
-		
-		$container.find('input').keydown(function(e){
-			var $this_=$(this);
+		var this_=this;//绑定原型对象this
+		this.$container.find('input').keydown(function(e){
+			var $this_=$(this);//保存inputh对象
 			var keydownId;
 			clearTimeout(keydownId);
 			keydownId = setTimeout(function(){
@@ -79,33 +148,33 @@ var Mailbox=
 				
 				//输入分号事件
 				if(keyCode == 186){
-					$container.find('input').change()
+					this_.$container.find('input').change()
 					
 				}
 
 				//--------input框输入时动态改变长度-------------------
 				var testLength = $this_.val().length;
-				var input_width=parseInt(opts.style['input_width'].substring(0,3));
+				var input_width=parseInt(this_.opts.style['input_width'].substring(0,3));
 				var width=testLength*14>input_width?testLength*14:input_width;
 				$this_.css('width', width + 'px')
 				
 				if(testLength>0){
-					flag=false;//输入框非空，则flag为false
+					this_.flag=false;//输入框非空，则this.flag为false
 				}
 				//键盘按下delete按钮事件
 				if(keyCode == 8){
-					var $tip_click=$container.find('.tip_click');
-					var $tip=$container.find('.tip');
+					var $tip_click=this_.$container.find('.tip_click');
+					var $tip=this_.$container.find('.tip');
 					
 
 						if($tip_click.length>0){
 							$tip_click.remove();
 						}else{
 							if(testLength == 0){
-								if(flag){
+								if(this_.flag){
 									$tip.last().remove();
 								}else{
-									flag=true;
+									this_.flag=true;
 								}																	
 							}
 						}											
@@ -113,66 +182,6 @@ var Mailbox=
 			},100);
 		});
 	}
-//--------------内容改变具体的响应函数----------------------------------
-	function input(e){
-			var $this_=$(this);
-			var text=$this_.val();		
-			var tips="";
-			if(text.length>0){
-				if(/;/.test(text)){
-					var res=text.split(';');//分号;分组	
-				}else{
-					var res=[text];
-				}
-				$this_.val('')//清空
-				for(var i=0;i<res.length;i++){
-					if(res[i]!=""){
-						if(opts.validation(res[i])){
-							tips+='<div class="tip"><span class="message">'+res[i]+'</span><a class="del" href="javascript:void(0)" >x</a></div>'	
-						}else{
-							tips+='<div class="tip error"><span class="message">'+res[i]+'</span><a class="del error" href="javascript:void(0)" >x</a></div>'	
-					}
-						}
-				}
-				var $tips=$(tips);
-				$tips.appendTo($container.find('.added'));
-				$tips.each(function(index,ele){//$tips==>[div.tip,div.tip,…………]jquery数组
 
-					//div.tip小框样式设置	
-					$(ele).css(opts.style)
-
-					//tips的hover事件，边框颜色变化
-					$(ele).hover(function(){
-
-						$(ele).addClass('tip_hover');
-						$(ele).find('a').addClass('tip_hover');
-					},function(){
-						$(ele).removeClass('tip_hover');
-						$(ele).find('a').removeClass('tip_hover');
-					})
-					//tip的click事件
-					$(ele).click(function(event){
-						event.stopPropagation();//阻止冒泡
-						$container.find('input').focus()
-						if($(this).hasClass('tip_click')){
-							$(this).removeClass('tip_click');
-						}else{
-							$(this).addClass('tip_click');
-						}					
-					})					
-				})
-				//删除每个小tip框事件
-				$tips.find('.del').each(function(index,ele){
-					$(ele).click(function(event){
-						$(this).closest('div.tip').remove();
-					})
-				})
-
-				//恢复输入框默认宽度
-				console.log(opts.style['input_width'])
-				$this_.css({'width':opts.style['input_width']});
-				flag=true;//此时输入框内容为空,flag置为true；
-			}
-	}
 	
 })(jQuery);
